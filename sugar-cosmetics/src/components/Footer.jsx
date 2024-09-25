2eb5281777dd879334e950256a05273d6cc64a58
manh610 <madridista6102000@gmail.com>
2024-09-25 08:05:02 +0700

    change url remote git - remove read more in footer

--- sugar-cosmetics/src/components/Footer.jsx
+++ sugar-cosmetics/src/components/Footer.jsx
@@ -20,21 +20,6 @@ export default function Footer() {
             "url(https://in.sugarcosmetics.com/desc-images/Rectangle_background.svg)",
         }}
       >
-        <a
-          style={{ textDecoration: "none", color: "#212121" }}
-          href="https://www.google.com/search?q=sugar+cosmetics+&rlz=1C1NMEO_enIN1000IN1000&sxsrf=ALiCzsZZ0rszGELi5SXoAFm5WiPjMrufuQ%3A1666943136306&ei=oIhbY4ekEvaQseMP7be6oAs&ved=0ahUKEwiH9P-Lt4L7AhV2SGwGHe2bDrQQ4dUDCA8&uact=5&oq=sugar+cosmetics+&gs_lp=Egdnd3Mtd2l6uAED-AEBMgQQIxgnMgQQIxgnMgQQIxgnMggQABiABBixAzIFEAAYgAQyCBAAGIAEGLEDMgsQLhiABBjHARivATIIEAAYgAQYsQMyCBAAGIAEGMkDMgUQABiABMICChAAGEcY1gQYsAOQBgdItA5Q9wpY9wpwAngByAEAkAEAmAGOAaABjgGqAQMwLjHiAwQgTRgB4gMEIEEYAOIDBCBGGACIBgE&sclient=gws-wiz"
-        >
-          READ MORE ABOUT SUGAR COSMETICS
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="red"
-            width="20"
-            height="18"
-            viewBox="0 0 384 512"
-          >
-            <path d="M3.4 81.7c-7.9 15.8-1.5 35 14.3 42.9L280.5 256 17.7 387.4C1.9 395.3-4.5 414.5 3.4 430.3s27.1 22.2 42.9 14.3l320-160c10.8-5.4 17.7-16.5 17.7-28.6s-6.8-23.2-17.7-28.6l-320-160c-15.8-7.9-35-1.5-42.9 14.3z" />
-          </svg>
-        </a>
       </div>
       <div
         style={{
